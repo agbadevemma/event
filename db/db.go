876e73ddc7436bc89476d96bd1a130c88a8e0abb
Emmanuel <agbadevemmanuel@gmail.com>
2026-02-01 01:12:48 +0100

    feat(db): add registrations table to support event sign-ups
    Create a new table to track user registrations for events, linking users and events via foreign keys.

--- db/db.go
+++ db/db.go
@@ -59,4 +59,18 @@ func CreateTable() {
 		panic("Could not create event DB")
 	}
 
+	createRegistrationTable :=
+		`CREATE TABLE IF NOT EXISTS registrations (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	event_id INTEGER,
+	user_id INTEGER,
+	FOREIGN KEY(event_id) REFERENCES events(id),
+	FOREIGN key(user_id) REFERENCES users(id)
+	);`
+	_, err = DB.Exec(createRegistrationTable)
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not create event DB")
+	}
+
 }
